260e277a4feb42f959b572479bd988c73aefb5ee
David Dobervich <david.dobervich@gmail.com>
2017-09-22 10:46:48 -0700

    add all my code

---
+++ src/Game.java
@@ -0,0 +1,57 @@
+import javax.swing.JOptionPane;
+
+public class Game {
+	public static void main(String[] args) {
+
+		int guess, secret;
+
+		do {
+			secret = 7;
+
+			String response;
+			response = JOptionPane.showInputDialog("Type a number");
+			guess = Integer.parseInt(response);
+
+			if (guess == secret) {
+				System.out.println("You got it!");
+			}
+
+			if (guess != secret) {
+				System.out.println("Nope.");
+			}
+
+		} while (guess != secret);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
